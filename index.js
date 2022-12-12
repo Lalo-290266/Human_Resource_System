e94a09577ff30d4e8edb3d100aa4d95ca95e902d
Eduardo Martinez <em552263@gmail.com>
2022-12-11 18:43:00 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const { response, request } = require('express');
+const express = require('express');
+const app = express();
+
+app.get("/", (response, request, next) => {
+    response.status(200).send('Bienvenido!');
+});
+
+app.listen(process.env.PORT || 3000, () => {//Server
+    console.log('Server is runnig...');
+});
